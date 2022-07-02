068ca2a1454444df714d1640d94dd113eedce13f
quanhspdz <pc.quanhdz@gmail.com>
2022-07-02 08:49:36 +0700

    setup CropImageActivity

--- app/src/main/java/com/example/instaclone/PostActivity.java
+++ app/src/main/java/com/example/instaclone/PostActivity.java
@@ -1,14 +1,61 @@
 package com.example.instaclone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class PostActivity extends AppCompatActivity {
+
+    TextView txtCancel, txtPost;
+    ImageView addedImage;
+    SocialAutoCompleteTextView edtDescription;
+    private Uri addedImgUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        txtCancel = findViewById(R.id.txtCancel);
+        txtPost = findViewById(R.id.txtPost);
+        addedImage = findViewById(R.id.img_added);
+        edtDescription = findViewById(R.id.edtDescription);
+
+        txtCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PostActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        CropImage.activity().start(PostActivity.this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            assert result != null;
+            addedImgUri = result.getUri();
+            addedImage.setImageURI(addedImgUri);
+        } else {
+            Toast.makeText(this, "Error, please try again!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(PostActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
