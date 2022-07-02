c47977ff6337b9b258bfe84202eed6e68af62636
quanhspdz <pc.quanhdz@gmail.com>
2022-07-02 09:50:42 +0700

    setup uploadImage function: upload post & hashtags to Firebase Database

--- app/src/main/java/com/example/instaclone/PostActivity.java
+++ app/src/main/java/com/example/instaclone/PostActivity.java
@@ -1,18 +1,35 @@
 package com.example.instaclone;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -20,6 +37,7 @@ public class PostActivity extends AppCompatActivity {
     ImageView addedImage;
     SocialAutoCompleteTextView edtDescription;
     private Uri addedImgUri;
+    private String imgUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +58,83 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+        txtPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
         CropImage.activity().start(PostActivity.this);
+    }
+
+    private void uploadImage() {
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading...");
+        progressDialog.show();
+
+        if (addedImgUri != null) {
+            //get image name & extension
+            StorageReference filePath = FirebaseStorage.getInstance().getReference("Posts")
+                    .child(System.currentTimeMillis() + "." + getFileExtension(addedImgUri));
+
+            //get image url
+            StorageTask uploadTask = filePath.putFile(addedImgUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return filePath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    //upload post
+                    Uri downloadUri = (Uri) task.getResult();
+                    imgUrl = downloadUri.toString();
+
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+                    String postId = ref.push().getKey();
+
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("postId", postId);
+                    map.put("imageUrl", imgUrl);
+                    map.put("descriptions", edtDescription.getText().toString());
+                    map.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                    assert postId != null;
+                    ref.child(postId).setValue(map);
+
+                    //get hashtags & upload them to firebaseDatabase
+                    DatabaseReference hashtagsRef = FirebaseDatabase.getInstance().getReference().child("HashTags");
+                    List<String> hashTags = edtDescription.getHashtags();
+                    if (hashTags != null) {
+                        if (!hashTags.isEmpty()) {
+                            for (String tag : hashTags) {
+                                map.clear();
+                                map.put("tag", tag);
+                                map.put("postId", postId);
+                                hashtagsRef.child(tag.toLowerCase()).setValue(map);
+                            }
+                        }
+                    }
+
+                    progressDialog.dismiss();
+                    Intent intent = new Intent(PostActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(PostActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "No image was selected!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -57,5 +151,12 @@ public class PostActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+    }
+
+    public String getFileExtension(Uri uri) {
+        ContentResolver cR = getApplicationContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 }
