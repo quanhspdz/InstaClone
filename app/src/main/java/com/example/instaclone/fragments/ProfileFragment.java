6b5e796036a1611a8a2647de5d8ab00e8895842a
quanhspdz <pc.quanhdz@gmail.com>
2022-09-18 22:22:32 +0700

    show list of saved posts when user tap saved-posts-button

--- app/src/main/java/com/example/instaclone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instaclone/fragments/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +28,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -38,11 +40,14 @@ public class ProfileFragment extends Fragment {
     private AppCompatButton btnEditProfile;
     private ImageButton btnMyPosts, btnSavedPosts;
     private RecyclerView recyclerViewMyPost, recyclerViewSavedPosts;
+    private FrameLayout frameMyPosts, frameSavedPosts;
 
     private GridPostAdapter myPostsAdapter, savedPostedAdapter;
     private List<Post> listMyPosts, listSavePosts;
     private GridLayoutManager gridLayoutManager;
 
+    private HashMap<String, Post> mapSavedPostsId;
+    private List<String> listSavedPostId;
     private String userId;
 
     @Override
@@ -64,19 +69,134 @@ public class ProfileFragment extends Fragment {
         btnSavedPosts = view.findViewById(R.id.btn_mySavedPosts);
         recyclerViewMyPost = view.findViewById(R.id.recycler_view_my_posts);
         recyclerViewSavedPosts = view.findViewById(R.id.recycler_view_saved_posts);
+        frameMyPosts = view.findViewById(R.id.frame_myPosts);
+        frameSavedPosts = view.findViewById(R.id.frame_savedPosts);
 
         userId = FirebaseAuth.getInstance().getUid();
 
         listMyPosts = new ArrayList<>();
         listSavePosts = new ArrayList<>();
+        listSavedPostId = new ArrayList<>();
+        mapSavedPostsId = new HashMap<>();
 
-        setupRecyclerView();
+        setupSwitchButton();
+
+        setupRecyclerViewMyPosts();
         setUserProfileData();
 
         return view;
     }
 
-    private void setupRecyclerView() {
+    private void setupSwitchButton() {
+        //firstly, it automatically show user's posts
+        btnMyPosts.setTag("isChosen");
+        btnMyPosts.setImageResource(R.drawable.ic_grid_selected);
+        btnSavedPosts.setTag("isNotChosen");
+        btnSavedPosts.setImageResource(R.drawable.ic_save_blur);
+
+        //when user tap 1 of 2 switch buttons highlight it and blur the other
+        frameMyPosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBtnMyPostsIsChosen();
+            }
+        });
+        frameSavedPosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBtnSavedPostIsChosen();
+            }
+        });
+
+        //surprise! this seem a dumb duplicated but it will set all view in frameLayout onClickListener
+        btnMyPosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBtnMyPostsIsChosen();
+            }
+        });
+        btnSavedPosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBtnSavedPostIsChosen();
+            }
+        });
+    }
+
+    private void onBtnMyPostsIsChosen() {
+        btnMyPosts.setTag("isChosen");
+        btnMyPosts.setImageResource(R.drawable.ic_grid_selected);
+        btnSavedPosts.setTag("isNotChosen");
+        btnSavedPosts.setImageResource(R.drawable.ic_save_blur);
+
+        recyclerViewMyPost.setVisibility(View.VISIBLE);
+        recyclerViewSavedPosts.setVisibility(View.GONE);
+    }
+
+    private void onBtnSavedPostIsChosen() {
+        btnSavedPosts.setTag("isChosen");
+        btnSavedPosts.setImageResource(R.drawable.ic_save);
+        btnMyPosts.setTag("isNotChosen");
+        btnMyPosts.setImageResource(R.drawable.ic_my_posts);
+
+        recyclerViewSavedPosts.setVisibility(View.VISIBLE);
+        recyclerViewMyPost.setVisibility(View.GONE);
+
+        setupRecyclerViewSavedPosts();
+    }
+
+    private void setupRecyclerViewSavedPosts() {
+        savedPostedAdapter = new GridPostAdapter(getContext(), listSavePosts);
+        recyclerViewSavedPosts.setHasFixedSize(true);
+        recyclerViewSavedPosts.setAdapter(savedPostedAdapter);
+        gridLayoutManager = new GridLayoutManager(getContext(), 3, GridLayoutManager.VERTICAL, false);
+        recyclerViewSavedPosts.setLayoutManager(gridLayoutManager);
+
+        //get list of saved posts id from Firebase
+        mapSavedPostsId.clear();
+        listSavedPostId.clear();
+        FirebaseDatabase.getInstance().getReference()
+                .child("SavedPosts")
+                .child(userId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            mapSavedPostsId.put(dataSnapshot.getKey(), null);
+                            listSavedPostId.add(dataSnapshot.getKey());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        //now from list of postId get post data from Firebase
+        listSavePosts.clear();
+        FirebaseDatabase.getInstance().getReference()
+                .child("Posts")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            if (mapSavedPostsId.containsKey(dataSnapshot.getKey())) {
+                                Post post = dataSnapshot.getValue(Post.class);
+                                listSavePosts.add(post);
+                            }
+                        }
+                        savedPostedAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void setupRecyclerViewMyPosts() {
         myPostsAdapter = new GridPostAdapter(getContext(), listMyPosts);
         recyclerViewMyPost.setHasFixedSize(true);
         recyclerViewMyPost.setAdapter(myPostsAdapter);
