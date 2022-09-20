46a677987923c7b470fc14ccd59a027e6bf9badb
quanhspdz <pc.quanhdz@gmail.com>
2022-09-20 16:33:05 +0700

    design edit-profile-activity

---
+++ app/src/main/java/com/example/instaclone/activities/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.instaclone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.instaclone.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
